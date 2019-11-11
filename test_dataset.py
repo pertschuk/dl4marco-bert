41e67877cefd8234d06493c143a64347fd986e3b
Jack Pertschuk <jp954@cornell.edu>
2019-11-10 22:26:40 -0500

    .

--- test_dataset.py
+++ test_dataset.py
@@ -57,8 +57,8 @@ def main():
         },
         drop_remainder=True)
     add_to_q('data/top1000.dev')
-    for og_features, new_features in zip(og_dataset.make_one_shot_iterator().get_next(),
-                                         dataset.make_one_shot_iterator().get_next()):
+    for og_features, new_features in zip(og_dataset.make_one_shot_iterator(),
+                                         dataset.make_one_shot_iterator()):
         print(og_features)
         print(new_features)
         assert og_features == new_features
