4d87c9e8fc3ee824d758d4d914600375a747337d
Jack Pertschuk <jp954@cornell.edu>
2019-11-11 03:51:14 -0500

    .

--- run_msmarco.py
+++ run_msmarco.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from server import feature_generator, input_q
-from test_dataset import add_to_q
 from threading import Thread
 from queue import Queue
 import collections
@@ -12,113 +11,22 @@ import csv
 
 import os
 import time
-
 import numpy as np
 import tensorflow as tf
-
-# local modules
-import metrics
 import modeling
-import optimization
-
-flags = tf.flags
 
 SEQ_LENGTH = 512
 
-FLAGS = flags.FLAGS
-
 output_q = Queue()
 
-## Required parameters
-flags.DEFINE_string(
-    "data_dir", 
-    "./data/tfrecord/",
-    "The input data dir. Should contain the .tfrecord files and the supporting "
-    "query-docids mapping files.")
-
-flags.DEFINE_string(
-    "bert_config_file",
-    "./data/bert/pretrained_models/uncased_L-24_H-1024_A-16/bert_config.json",
-    "The config json file corresponding to the pre-trained BERT model. "
-    "This specifies the model architecture.")
-
-flags.DEFINE_string(
-    "output_dir", "./data/output",
-    "The output directory where the model checkpoints will be written.")
-
-flags.DEFINE_boolean(
-    "msmarco_output", True,
-    "Whether to write the predictions to a MS-MARCO-formatted file.")
-
-flags.DEFINE_string(
-    "init_checkpoint",
-    "./data/bert/pretrained_models/uncased_L-24_H-1024_A-16/bert_model.ckpt",
-    "Initial checkpoint (usually from a pre-trained BERT model).")
-
-flags.DEFINE_integer(
-    "max_seq_length", 512,
-    "The maximum total input sequence length after WordPiece tokenization. "
-    "Sequences longer than this will be truncated, and sequences shorter "
-    "than this will be padded.")
-
-flags.DEFINE_bool("do_train", True, "Whether to run training.")
-
-flags.DEFINE_bool("do_eval", True, "Whether to run eval on the dev set.")
-
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
-
-flags.DEFINE_integer("eval_batch_size", 32, "Total batch size for eval.")
-
-flags.DEFINE_float("learning_rate", 1e-6, "The initial learning rate for Adam.")
-
-flags.DEFINE_integer("num_train_steps", 400000,
-                     "Total number of training steps to perform.")
-
-flags.DEFINE_integer("max_eval_examples", None,
-                     "Maximum number of examples to be evaluated.")
-
-flags.DEFINE_integer("num_eval_docs", 1000,
-                     "Number of docs per query in the dev and eval files.")
-
-flags.DEFINE_integer(
-    "num_warmup_steps", 40000,
-    "Number of training steps to perform linear learning rate warmup.")
-
-flags.DEFINE_integer("save_checkpoints_steps", 1000,
-                     "How often to save the model checkpoint.")
-
-flags.DEFINE_integer("iterations_per_loop", 1000,
-                     "How many steps to make in each estimator call.")
-
-flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
-
-tf.flags.DEFINE_string(
-    "tpu_name", None,
-    "The Cloud TPU to use for training. This should be either the name "
-    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
-    "url.")
-
-tf.flags.DEFINE_string(
-    "tpu_zone", None,
-    "[Optional] GCE zone where the Cloud TPU is located in. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
-
-tf.flags.DEFINE_string(
-    "gcp_project", None,
-    "[Optional] Project name for the Cloud TPU-enabled project. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
-
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
-
-flags.DEFINE_integer(
-    "num_tpu_cores", 8,
-    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
-
+bert_config_file = 'bert_marco/bert_config.json'
+init_ckpt = 'bert_marco/bert_model.ckpt'
+model_dir = 'bert_marco/'
+batch_size = 4
+num_eval_docs = 1000
 
 def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
-                 labels, num_labels, use_one_hot_embeddings):
+                 labels, num_labels):
   """Creates a classification model."""
   model = modeling.BertModel(
       config=bert_config,
@@ -126,7 +34,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
       input_ids=input_ids,
       input_mask=input_mask,
       token_type_ids=segment_ids,
-      use_one_hot_embeddings=use_one_hot_embeddings)
+      use_one_hot_embeddings=False)
 
   output_layer = model.get_pooled_output()
   hidden_size = output_layer.shape[-1].value
@@ -155,9 +63,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
     return (loss, per_example_loss, log_probs)
 
 
-def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
-                     num_train_steps, num_warmup_steps, use_tpu,
-                     use_one_hot_embeddings):
+def model_fn_builder(bert_config, num_labels, init_checkpoint):
   """Returns `model_fn` closure for TPUEstimator."""
 
   def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
@@ -171,30 +77,22 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
     is_training = (mode == tf.estimator.ModeKeys.TRAIN)
     (total_loss, per_example_loss, log_probs) = create_model(
         bert_config, is_training, input_ids, input_mask, segment_ids, label_ids,
-        num_labels, use_one_hot_embeddings)
+        num_labels)
 
     tvars = tf.trainable_variables()
 
     scaffold_fn = None
     (assignment_map, initialized_variable_names
     ) = modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
-    if use_tpu:
 
-      def tpu_scaffold():
-        tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-        return tf.train.Scaffold()
+    tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
 
-      scaffold_fn = tpu_scaffold
-    else:
-      tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-
-    output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+    output_spec = tf.estimator.EstimatorSpec(
       mode=mode,
       predictions={
         "log_probs": log_probs,
         "label_ids": label_ids,
-      },
-      scaffold_fn=scaffold_fn)
+      })
     return output_spec
 
   return model_fn
@@ -234,44 +132,22 @@ def input_fn(params):
 
 def main(_):
 
-  bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
+  bert_config = modeling.BertConfig.from_json_file(bert_config_file)
   assert SEQ_LENGTH <= bert_config.max_position_embeddings
 
-  tpu_cluster_resolver = None
-  if FLAGS.use_tpu and FLAGS.tpu_name:
-    tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
-        FLAGS.tpu_name, zone=FLAGS.tpu_zone, project=FLAGS.gcp_project)
-
-  is_per_host = tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
-  run_config = tf.contrib.tpu.RunConfig(
-      cluster=tpu_cluster_resolver,
-      master=FLAGS.master,
-      model_dir=FLAGS.output_dir,
-      save_checkpoints_steps=FLAGS.save_checkpoints_steps,
-      tpu_config=tf.contrib.tpu.TPUConfig(
-          iterations_per_loop=FLAGS.iterations_per_loop,
-          num_shards=FLAGS.num_tpu_cores,
-          per_host_input_for_training=is_per_host))
+  run_config = tf.estimator.RunConfig(
+      model_dir=model_dir)
 
   model_fn = model_fn_builder(
       bert_config=bert_config,
       num_labels=2,
-      init_checkpoint=FLAGS.init_checkpoint,
-      learning_rate=FLAGS.learning_rate,
-      num_train_steps=FLAGS.num_train_steps,
-      num_warmup_steps=FLAGS.num_warmup_steps,
-      use_tpu=FLAGS.use_tpu,
-      use_one_hot_embeddings=FLAGS.use_tpu)
+      init_checkpoint=init_ckpt)
 
-  # If TPU is not available, this will fall back to normal Estimator on CPU
-  # or GPU.
-  estimator = tf.contrib.tpu.TPUEstimator(
-      use_tpu=FLAGS.use_tpu,
+  estimator = tf.estimator.Estimator(
       model_fn=model_fn,
       config=run_config,
-      train_batch_size=FLAGS.train_batch_size,
-      eval_batch_size=FLAGS.eval_batch_size,
-      predict_batch_size=FLAGS.eval_batch_size)
+      eval_batch_size=batch_size,
+      predict_batch_size=batch_size)
 
   result = estimator.predict(input_fn=input_fn,
                              yield_single_examples=True)
@@ -281,7 +157,7 @@ def main(_):
   for item in result:
     results.append((item["log_probs"], item["label_ids"]))
 
-    if len(results) == FLAGS.num_eval_docs:
+    if len(results) == num_eval_docs:
 
       log_probs, labels = zip(*results)
       log_probs = np.stack(log_probs).reshape(-1, 2)
@@ -305,11 +181,12 @@ def pad_docs(docs, eval_size):
 def output_fn():
     eval_size = 1000
     qrels = set()
+
     with open(os.path.join('data', 'qrels.dev.small.tsv')) as fh:
         data = csv.reader(fh, delimiter='\t')
         for qid, _, doc_id, _ in data:
             qrels.add((qid, doc_id))
-    i = 0
+
     qid_count = collections.defaultdict(int)
     query_dict = dict()
     docs_dict = collections.defaultdict(list)
