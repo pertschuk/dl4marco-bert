dcc1a9481470ccf69699927a0adc38289714f726
Jack Pertschuk <jp954@cornell.edu>
2019-11-11 03:52:38 -0500

    .

--- run_msmarco.py
+++ run_msmarco.py
@@ -146,8 +146,7 @@ def main(_):
   estimator = tf.estimator.Estimator(
       model_fn=model_fn,
       config=run_config,
-      eval_batch_size=batch_size,
-      predict_batch_size=batch_size)
+      batch_size=batch_size)
 
   result = estimator.predict(input_fn=input_fn,
                              yield_single_examples=True)
