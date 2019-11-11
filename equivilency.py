4e7718915efef0ea0ee3fde60eb6c3a84b25f7fb
Jack Pertschuk <jp954@cornell.edu>
2019-11-10 21:01:16 -0500

    .

--- equivilency.py
+++ equivilency.py
@@ -15,18 +15,17 @@ def main():
         data = csv.reader(fh, delimiter='\t')
         for qid, _, doc_id, _ in data:
             qrels.add((qid, doc_id))
-            qid_count[qid] += 1
 
     with open(os.path.join(DATA_PATH, 'output', 'msmarco_predictions_dev.tsv')) as fh:
         data = csv.reader(fh, delimiter='\t')
         for qid, doc_id, rank in data:
             if (qid, doc_id) in qrels:
                 print(qid, ' ', rank)
-                qid_count[qid] = max(qid_count[qid], (1.0 / float(rank)))
-            mrr = sum(qid_count.values()) / len(qid_count.keys())
-            print(sum(qid_count.values()))
-            print(len(qid_count.keys()))
-            print("MRR: %s " % mrr)
+                qid_count[qid] = max(qid_count[qid], (1.0 / (float(rank))))
+                mrr = sum(qid_count.values()) / len(qid_count.keys())
+                print(sum(qid_count.values()))
+                print(len(qid_count.keys()))
+                print("MRR: %s " % mrr)
 
 if __name__== '__main__':
     main()
